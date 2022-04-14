3207ec0110ffbb0ff70525efd251b5e3df025fbd
SigmanautNZ <103622932+SigmanautNZ@users.noreply.github.com>
2022-04-14 17:09:13 +1200

    Add files via upload
    Add all initial files

---
+++ Atelier-Seakittens-Web/Build Deliverables/Javascript/Script(Index).js
@@ -0,0 +1,40 @@
+'use strict';
+
+// Confirmation Clicks for each Navbar Element
+
+const AboutButton = document.querySelector('#aboutme');
+const PortfolioButton = document.querySelector('#portfolio');
+const AwardsButton = document.querySelector('#awards');
+const ContactMeButton = document.querySelector('#contactme');
+
+AboutButton.addEventListener('click', () => AboutButton.style.backgroundColor = '#FFD700')
+
+PortfolioButton.addEventListener('click', () => PortfolioButton.style.backgroundColor = '#FFD700')
+
+AwardsButton.addEventListener('click', () => AwardsButton.style.backgroundColor = '#FFD700')
+
+ContactMeButton.addEventListener('click', () => ContactMeButton.style.backgroundColor = '#FFD700')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
